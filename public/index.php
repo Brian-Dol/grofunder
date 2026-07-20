b5ce142d2cf7133370f1e1b70b4421cba5e06941
Growfunder Admin <admin@growfunder.local>
2026-07-20 10:21:15 +0300

    Fix: Use correct lowercase path for HttpsAssetHelper.php loader
    - Change app/Helpers/ to app/helpers/ (case-sensitive on Linux)
    - Ensures helper is loaded correctly on Render deployment

--- public/index.php
+++ public/index.php
@@ -42,7 +42,7 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
-require __DIR__.'/../app/Helpers/HttpsAssetHelper.php';
+require __DIR__.'/../app/helpers/HttpsAssetHelper.php';
 
 /*
 |--------------------------------------------------------------------------
