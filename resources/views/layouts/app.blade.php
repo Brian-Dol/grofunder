974bd853b4cfe174f50c69a5e8eae2b60343c3c0
Growfunder Admin <admin@growfunder.local>
2026-07-27 13:47:20 +0300

    FIX: Add @vite directive for Tailwind CSS - load compiled assets from Vite build

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
 
       <!--Bootstrap-->
       <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
