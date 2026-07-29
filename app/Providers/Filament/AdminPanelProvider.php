0d94304a4a7c2573d06d4f108109644239de3356
Growfunder Admin <admin@growfunder.local>
2026-07-29 13:24:22 +0300

    test: Disable Pages and Widgets discovery

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -52,11 +52,11 @@ class AdminPanelProvider extends PanelProvider
             ->resources([
                 // BorrowerResource::class, // TEMPORARILY DISABLED FOR TESTING
             ])
-            ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
+            // ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
                 Dashboard::class,
             ])
-            ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
+            // ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([])
             ->navigationItems([
                 NavigationItem::make('Statement of Financial Position')
