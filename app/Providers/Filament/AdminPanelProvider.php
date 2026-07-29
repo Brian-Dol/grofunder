488f9fe53e823ee3fa1eeceee8c66d911b1f5d15
Growfunder Admin <admin@growfunder.local>
2026-07-29 11:27:50 +0300

    restore: Re-enable Filament resource/page/widget discovery - was disabled during debugging but never restored

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -46,9 +46,12 @@ class AdminPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Amber,
             ])
+            ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
+            ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
                 Dashboard::class,
             ])
+            ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([])
             ->navigationItems([
                 NavigationItem::make('Statement of Financial Position')
